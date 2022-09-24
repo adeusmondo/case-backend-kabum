3a8bcf304f8ea5c456d52f414b0d80480ba435dc
Carlos Mondo <csvmondo@gmail.com>
2022-09-24 16:57:28 -0300

    feat: new shipping service for business logics

---
+++ src/shipping/services/shipping_service.py
@@ -0,0 +1,54 @@
+from decimal import Decimal
+
+from logger import Logger
+
+
+class ShippingService:
+    def __init__(self, logger: Logger):
+        self.logger = logger
+
+    @staticmethod
+    def retrieve_shipping_options():
+        '''
+        This method is used to retrieve the shipping options available for the user
+        when database is not available
+        '''
+        return [
+            dict(name='Entrega Ninja',
+                 min_height=10,
+                 max_height=200,
+                 min_width=6,
+                 max_width=140,
+                 delivery_deadline=6,
+                 constant_shipping_calculation=0.3),
+            dict(name='Entrega KaBuM',
+                 min_height=5,
+                 max_height=140,
+                 min_width=13,
+                 max_width=125,
+                 delivery_deadline=4,
+                 constant_shipping_calculation=0.2)
+        ]
+
+    def calculating_shipping_cost(self, shipping_option, product):
+        cost = Decimal((product['weight'] * shipping_option['constant_shipping_calculation']) / 10)
+        shipping_option['shipping_cost'] = cost
+
+        return shipping_option
+
+    async def validate_product_dimensions(self, product):
+        '''
+        This method is used to validate the product dimensions
+        '''
+        print('aaaa')
+        options = []
+        product_dimensions = product['dimension']
+        shipping_options = self.retrieve_shipping_options()
+        self.logger.debug(f'Following shipping options are available: {shipping_options}')
+
+        for shipping_option in shipping_options:
+            if (shipping_option['min_height'] <= product_dimensions['height'] <= shipping_option['max_height'] and
+                    shipping_option['min_width'] <= product_dimensions['width'] <= shipping_option['max_width']):
+                options.append(self.calculating_shipping_cost(shipping_option, product))
+
+        return options
